567c0ecd0e9162771fcce6297d09050d5415502e
Glitch (hello-express) <none>
2020-01-28 23:29:09 +0000

    👰🚀 Checkpoint ./app.js:10779727/5261 ./package.json:10779727/891

--- app.js
+++ app.js
@@ -6,15 +6,64 @@ const app = new App({
   signingSecret: process.env.SLACK_SIGNING_SECRET
 });
 
+app.event("channel_created", async ({ event, context }) => {
+  try {
+    /* view.publish is the method that your app uses to push a view to the Home tab */
+    const result = await app.client.views.publish({
+      /* retrieves your xoxb token from context */
+      token: context.botToken,
 
+      /* the user that opened your app's app home */
+      user_id: event.channel,
 
-// All the room in the world for your code
+      /* the view payload that appears in the app home*/
+      view: {
+        type: "home",
+        callback_id: "home_view",
 
-
+        /* body of the view */
+        blocks: [
+          {
+            type: "section",
+            text: {
+              type: "mrkdwn",
+              text: "*Welcome to your _App's Home_* :tada:"
+            }
+          },
+          {
+            type: "divider"
+          },
+          {
+            type: "section",
+            text: {
+              type: "mrkdwn",
+              text:
+                "This button won't do much for now but you can set up a listener for it using the `actions()` method and passing its unique `action_id`. See an example in the `examples` folder within your Bolt app."
+            }
+          },
+          {
+            type: "actions",
+            elements: [
+              {
+                type: "button",
+                text: {
+                  type: "plain_text",
+                  text: "Click me!"
+                }
+              }
+            ]
+          }
+        ]
+      }
+    });
+  } catch (error) {
+    console.error(error);
+  }
+});
 
 (async () => {
   // Start your app
   await app.start(process.env.PORT || 3000);
 
-  console.log('⚡️ Bolt app is running!');
+  console.log("⚡️ Bolt app is running!");
 })();
