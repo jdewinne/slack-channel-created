0ac7085b7b957e1c9ea2ae17154390abd261d1b4
Joris De Winne <joris.dewinne@gmail.com>
2021-04-09 13:54:34 -0700

    Adding healtcheck endpoint for the bolt app

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 // Require the Bolt package (github.com/slackapi/bolt)
-const { App } = require("@slack/bolt");
+const { App, ExpressReceiver } = require("@slack/bolt");
 var _ = require("lodash");
 var events = [];
+
+// Create a Bolt Receiver
+const receiver = new ExpressReceiver({ signingSecret: process.env.SLACK_SIGNING_SECRET });
 
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
+  receiver: receiver
 });
 
 app.event("channel_created", ({ event, context }) => {
@@ -25,6 +29,13 @@ app.event("channel_created", ({ event, context }) => {
     console.error(error);
   }
 });
+
+// Other web requests are methods on receiver.router
+receiver.router.get('/', (req, res) => {
+  // You're working with an express req and res now.
+  res.status(200).send();
+});
+
 
 (async () => {
   // Start your app
