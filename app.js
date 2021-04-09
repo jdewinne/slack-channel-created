4714cac99b8cbcd418af2ec93cdbf176037ef836
Joris De Winne <joris.dewinne@gmail.com>
2021-04-08 21:34:30 -0700

    GKE version part 1

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var events = [];
 
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
-  signingSecret: process.env.SLACK_SIGNING_SECRET
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
 });
 
 app.event("channel_created", ({ event, context }) => {
